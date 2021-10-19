c2fe4367a34cd956e25dcb35b440955054f1eb82
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-10-19 13:51:53 +0900

    feat: add home components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ViewHomeComponent } from './views/view-home/view-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewHomeComponent
   ],
   imports: [
     BrowserModule,
